bef53ccf3969ff8dae221260d43f0fc6c57453ef
Joel Allen <joelallen720@gmail.com>
2016-08-07 10:50:46 -0700

    Add .babelrc and webpack.config.js.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const webpack    = require('webpack');
+const fs         = require('fs');
+const path       = require('path');
+const hjsWebpack = require('hjsWebpack');
+
+const join    = path.join;
+const resolve = path.resolve;
+
+const ROOT = resolve(__dirname);
+
+module.exports = hjsWebpack({
+    in               : join(ROOT, 'src/app.js'),
+    out              : join(ROOT, 'dist'),
+    isDev            : process.env.NODE_ENV === 'development',
+    clearBeforeBuild : true,
+});
