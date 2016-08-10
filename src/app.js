b7a3f9f09e8cef2d16e777ddfa616a5cd5d911a7
Joel Allen <joelallen720@gmail.com>
2016-08-10 10:41:59 -0700

    Use font-awesome in app.

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 
 import styles from './styles.module.css'
 import './app.css'
+import 'font-awesome/css/font-awesome.css'
 
 const App = React.createClass({
     render: () => (
-        <div className={ styles.container }>
-            <h1>Environment: { __NODE_ENV__ }</h1>
+        <div className={ styles['container'] }>
+            <span>Environment: { __NODE_ENV__ }</span>
+            <span className='fa fa-star'></span>
         </div>
     ),
 });
